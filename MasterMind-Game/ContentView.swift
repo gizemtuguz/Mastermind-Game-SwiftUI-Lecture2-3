9d1bf2c034a3cec53d4fa372b7499d7bd78b387c
gizemtuguz <gizemtuguz@ogr.eskisehir.edu.tr>
2026-02-19 15:54:48 +0300

    Initial Commit

---
+++ MasterMind-Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasterMind-Game
+//
+//  Created by Gizem Tuğuz on 19.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasterMind-Game/MasterMind_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMind_GameApp.swift
+//  MasterMind-Game
+//
+//  Created by Gizem Tuğuz on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MasterMind_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
