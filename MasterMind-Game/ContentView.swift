3031bacf4674305ceb1bd2c1075da8aa77c1ce72
gizemtuguz <gizemtuguz@ogr.eskisehir.edu.tr>
2026-02-19 19:51:49 +0300

    Initial commit

--- MasterMind-Game/ContentView.swift
+++ MasterMind-Game/ContentView.swift
@@ -10,14 +10,58 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            pegs(colors: [.red, .green, .green, .yellow])
+            pegs(colors: [.red, .blue, .green, .red])
+            pegs(colors: [.red, .yellow, .green, .blue])
+            //pegs(colors: [.red, .green, .green])
         }
         .padding()
+
     }
+    /* var pegs: some View {
+        HStack{
+            Circle().foregroundStyle(.red)
+            Circle().foregroundStyle(.green)
+            Circle().foregroundStyle(.green)
+            Circle().foregroundStyle(.yellow)
+        }
+    } */
+    
+    func pegs(colors: Array<Color> = []) -> some View {
+        HStack{
+            ForEach(colors.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius:10)
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(colors[index])
+            }
+            //bu sayede index out of rage hatası kaldırıldı yukarıda 3 elemanlı bir öge de olsa compile edilebildi
+            
+            MatchMakers(matches: [.exact, .inexact, .nomatch, .exact])
+            
+            /*
+            HStack{
+                VStack{
+                    Circle().strokeBorder(Color.primary, lineWidth: 2) .aspectRatio(1, contentMode: .fit)
+                    Circle()
+                }
+                VStack{
+                    Circle()
+                    Circle()
+                }
+            }
+            */
+            /*
+            Circle().foregroundStyle(colors[0])
+            Circle().foregroundStyle(colors[1])
+            Circle().foregroundStyle(colors[2])
+            Circle().foregroundStyle(colors[3])
+            */
+            
+        }
+    }
+    
 }
+
 
 #Preview {
     ContentView()
--- MasterMind-Game/MatchMakers.swift
+++ MasterMind-Game/MatchMakers.swift
@@ -1,14 +1,41 @@
+//
+//  MatchMakers.swift
+//  MasterMind-Game
+//
+//  Created by Gizem Tuğuz on 19.02.2026.
+//
+import SwiftUI
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+        
+}
+
 struct MatchMakers: View {
+    var matches: [Match]
     var body: some View {
-        HStack{
-            VStack{
-                Circle().strokeBorder(Color.primary, lineWidth: 2) .aspectRatio(1, contentMode: .fit)
-                Circle()
+        VStack{
+            HStack{
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
             }
-            VStack{
-                Circle()
-                Circle()
+            HStack{
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
             }
         }
     }
+    func matchMarker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: { match in match == .exact })
+        let foundCount: Int = matches.count(where: { match in match != .nomatch })
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2).aspectRatio(1, contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMakers(matches: [.exact, .inexact, .nomatch, .exact])
 }
